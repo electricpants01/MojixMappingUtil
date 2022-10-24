19dba155f1d275c64ef8e0a11e7ffe7cdee42ee8
Christian Torrico Avila <chtorrico@ecisolutions.com>
2022-10-24 12:40:40 -0400

    first commit

---
+++ src/main/java/com/locotodevteam/mojixmapping/MojixMapping.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.locotodevteam.mojixmapping;
+
+/**
+ *
+ * @author Locoto
+ */
+public class MojixMapping {
+
+    public static void main(String[] args) {
+        String cad = "hola.como.estas";
+        String[] count = cad.split(".");
+        System.out.println("count " + count.length);
+    }
+}
